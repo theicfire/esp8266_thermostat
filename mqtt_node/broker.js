d0917534168948c32f6d1bbf9cc74981e53eab98
Chase Lambert <theicfire@gmail.com>
2022-02-13 23:40:48 -0800

    Change port

--- mqtt_node/broker.js
+++ mqtt_node/broker.js
@@ -1,6 +1,6 @@
 const aedes = require("aedes")();
 const server = require("net").createServer(aedes.handle);
-const port = 1883;
+const port = 2000;
 
 server.listen(port, function () {
   console.log(`MQTT Broker running on port: ${port}`);
